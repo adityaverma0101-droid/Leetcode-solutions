7aeb88e7ba870b4c7efd82799ff5beee44c67ef6
adityaverma0101-droid <aditya.verma01012006@gmail.com>
2026-08-17 00:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (38.21%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        int low=0;
+        int high=n-1;
+        int res=-1;
+        while(low<high){
+            int guess=(low+high)/2;
+            if(nums[guess]<nums[guess+1]){
+                low=guess+1;
+            }
+            else{
+                high=guess;
+            }
+        }
+        return low;
+
+    }
+}
