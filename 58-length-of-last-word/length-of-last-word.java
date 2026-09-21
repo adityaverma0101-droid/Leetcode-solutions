95516eb3495e31e455ea744a1bf503376c828c15
adityaverma0101-droid <aditya.verma01012006@gmail.com>
2026-09-21 14:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (83.46%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count=0;
+        boolean started=false;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i) != ' '){
+                count++;
+                started=true;
+            }
+            else if(started){
+                break;
+            }
+        }
+        return count;
+    }
+}
